9237a3c7705f43d4d9a0394061818f1606bdaec3
Edneide Ramalho <edneide.ramalho@gmail.com>
2021-08-06 18:46:03 -0300

    update

--- Python/004_Practical_statisticas_for_DS/001_script1.R
+++ Python/004_Practical_statisticas_for_DS/001_script1.R
@@ -2,3 +2,12 @@
 path_state <- "https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/state.csv"
 
 state <- read.csv(path_state)
+
+## Mean ------
+mean(state$Population)
+
+## Trimmed mean ----
+mean(state$Population, trim = 0.1)
+
+## Median ----
+median(state$Population)
