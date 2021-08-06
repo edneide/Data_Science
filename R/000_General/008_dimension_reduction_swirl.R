9fd359cb4b6d015bcbc0cea047a0550d7623abd8
Edneide Ramalho <edneide.ramalho@gmail.com>
2021-08-06 14:50:33 -0300

    Update

--- R/000_General/008_dimension_reduction_swirl.R
+++ R/000_General/008_dimension_reduction_swirl.R
@@ -5,3 +5,24 @@ Neide
 13
 heatmap(dataMatrix)
 myedit("addPatt.R")  
+
+2
+2
+1
+2
+source("addPatt.R", local = TRUE)
+
+heatmap(dataMatrix)
+2
+mat
+
+svd(mat)
+matu %*% diag %*% t(matv)
+
+svd(scale(mat))
+prcomp(scale(mat))
+svd1$v[,1]
+svd1$d
+
+head(constantMatrix)
+svd2$d
